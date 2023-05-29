087f1dad47f6a3518d5df038351e848f5fa6b684
LiMing924 <96522863+LiMing924@users.noreply.github.com>
2023-05-29 09:22:54 +0800

    删除不合理的枚举类型，后续待补充

--- liming/handle/connection/DriverClassName.java
+++ liming/handle/connection/DriverClassName.java
@@ -5,21 +5,43 @@ import liming.item.annotate.UtextAnnotate;
 @UtextAnnotate("配置jdbc版本")
 public enum DriverClassName {
     @UtextAnnotate("连接MySQL 8.x")
-    MySql8("com.mysql.cj.jdbc.Driver"),
+    MySql8(0, "com.mysql.cj.jdbc.Driver"),
     @UtextAnnotate("连接MySQL 5.x")
-    MySql5("com.mysql.jdbc.Driver"),
-    @UtextAnnotate("连接Oracle 11g")
-    Oracle11g("oracle.jdbc.driver.OracleDriver"),
-    @UtextAnnotate("连接SQL Server 2019")
-    SqlServer2019("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+    MySql5(1, "com.mysql.jdbc.Driver");
 
+    // @UtextAnnotate("连接Oracle 11g")
+    // Oracle11g("","oracle.jdbc.driver.OracleDriver"),
+    // @UtextAnnotate("连接SQL Server 2019")
+    // SqlServer2019("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+    private int num;
     private String value;
 
-    DriverClassName(String DriverClassName) {
+    DriverClassName(int num, String DriverClassName) {
+        this.num = num;
         this.value = DriverClassName;
     }
 
     public String getDriverClassName() {
         return value;
+    }
+
+    public static String getURL(SQL sql) {
+        switch (sql.Driver().num) {
+            case 0:
+            case 1:
+                return JDBC(sql);
+            default:
+                throw new RuntimeException();
+        }
+    }
+
+    private static String JDBC(SQL sql) {
+        return "jdbc:mysql://" + sql.hostname() + ":" + sql.port() + "/" + sql.databaseName()
+                + "?useUnicode=" + sql.useUnicode() + "&characterEncoding=" + sql.characterEncoding()
+                + "&autoReconnectForPools=" + sql.autoReconnectForPools() + "&autoReconnect="
+                + sql.autoReconnect() + "&failOverReadOnly=" + sql.failOverReadOnly()
+                + "&maxReconnects=" + sql.maxReconnects() + "&initialTimeout=" + sql.initialTimeout()
+                + "&connectTimeout=" + sql.connectTimeout() + "&socketTimeout=" + sql.socketTimeout();
+        // + "&serverTimezone=" + sql.serverTimezone();
     }
 }
